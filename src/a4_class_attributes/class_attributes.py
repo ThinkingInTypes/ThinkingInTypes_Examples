8f666ed95b3d21b4c443087c32efaedc68b43d9d
Bruce Eckel <mindviewinc@gmail.com>
2025-03-21 23:06:01 -0600

    mains removed

--- src/a4_class_attributes/class_attributes.py
+++ src/a4_class_attributes/class_attributes.py
@@ -14,17 +14,9 @@ class B:
         self.x = x_init
 
 
-if __name__ == "__main__":
-    a = A()
-    show(a, "a")
-    # [Class A] x: 100
-    # [Object a] Empty
-    a.x = 1
-    show(a, "a")
-    # [Class A] x: 100
-    # [Object a] x: 1
-
-    b = B(-99)
-    show(b, "b")
-    # [Class B] x: 100
-    # [Object b] x: -99
+a = A()
+show(a, "a")
+a.x = 1
+show(a, "a")
+b = B(-99)
+show(b, "b")
--- src/a4_class_attributes/it_all_goes_wrong.py
+++ src/a4_class_attributes/it_all_goes_wrong.py
@@ -19,46 +19,32 @@ def reset():
     A.y = 200
 
 
-if __name__ == "__main__":
-    a1: A = None
-    a2: A = None
-    a3: A = None
+a1: A = None
+a2: A = None
+a3: A = None
 
-    def display(counter: int):
-        print(f"display({counter})")
-        if a1:
-            print(f"{a1.x = }, {a1.y = }")
-        if a2:
-            print(f"{a2.x = }, {a2.y = }")
-        if a3:
-            print(f"{a3.x = }, {a3.y = }")
+def display(counter: int):
+    print(f"display({counter})")
+    if a1:
+        print(f"{a1.x = }, {a1.y = }")
+    if a2:
+        print(f"{a2.x = }, {a2.y = }")
+    if a3:
+        print(f"{a3.x = }, {a3.y = }")
 
-    a1 = A()
-    display(1)
-    # a1.x = 100, a1.y = 200
-    a1.x = -1
-    a1.y = -2
-    display(2)
-    # a1.x = -1, a1.y = -2
-    a1.change_x()
-    display(3)
-    # a1.x = -1, a1.y = -2
-    a2 = A()
-    display(4)
-    # a1.x = -1, a1.y = -2
-    # a2.x = 999, a2.y = 200
-    a2.y = 17
-    display(5)
-    # a1.x = -1, a1.y = -2
-    # a2.x = 999, a2.y = 17
-    A.change_y()
-    a3 = A()
-    display(6)
-    # a1.x = -1, a1.y = -2
-    # a2.x = 999, a2.y = 17
-    # a3.x = 999, a3.y = 313
-    reset()
-    display(7)
-    # a1.x = -1, a1.y = -2
-    # a2.x = 100, a2.y = 17
-    # a3.x = 100, a3.y = 200
+a1 = A()
+display(1)
+a1.x = -1
+a1.y = -2
+display(2)
+a1.change_x()
+display(3)
+a2 = A()
+display(4)
+a2.y = 17
+display(5)
+A.change_y()
+a3 = A()
+display(6)
+reset()
+display(7)
