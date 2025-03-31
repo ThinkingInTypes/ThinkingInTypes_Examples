6ee9f1464dd12582cd3aa2b2075e7781357722e7
Bruce Eckel <mindviewinc@gmail.com>
2025-03-30 22:08:09 -0600

    Update demo_exception_checker.py

--- a8_book_utilities/demo_exception_checker.py
+++ a8_book_utilities/demo_exception_checker.py
@@ -1,5 +1,6 @@
 # demo_exception_checker.py
 from dataclasses import dataclass
+
 from book_utils import Catch
 
 
@@ -18,48 +19,32 @@ def foo(a: int, b: Fob) -> str:
     return f"foo({a}, {b}) succeeded"
 
 
-def mark(marker) -> None:
-    print(f"[{marker}]:", end=" ")
-
-
 # If you know it succeeds you can just run it without a context:
-mark(1)
 print(foo(0, Fob(0)))
 ## foo(0, Fob(x=0)) succeeded
-
-mark(2)
 with Catch():  # Single-failure simple form
     foo(1, Fob(-1))
 ## Error: Fob arg -1 must be positive
 
 # In the simple form, success does NOT automatically display the result:
-mark(3)
 with Catch():
     print(foo(42, Fob(42)))  # Must explicitly print
 ## foo(42, Fob(x=42)) succeeded
-
-mark(4)
 with Catch() as catch:  # Lambda form displays successful result
     catch(lambda: foo(42, Fob(42)))
 ## foo(42, Fob(x=42)) succeeded
 
 # Multi-failure block requires lambda form:
 with Catch() as catch:
-    mark("A")
     catch(lambda: foo(1, Fob(1)))
-    mark("B")
     catch(lambda: foo(0, Fob(0)))
-    mark("C")
     catch(lambda: foo(-1, Fob(1)))
-    mark("D")
     catch(lambda: foo(1, Fob(-1)))
-    mark("E")
     catch(lambda: foo(-1, Fob(-1)))
-    mark("F")
     catch(lambda: foo(10, Fob(11)))
-## [A]: foo(1, Fob(x=1)) succeeded
-## [B]: foo(0, Fob(x=0)) succeeded
-## [C]: Error: foo arg -1 must be positive
-## [D]: Error: Fob arg -1 must be positive
-## [E]: Error: Fob arg -1 must be positive
-## [F]: foo(10, Fob(x=11)) succeeded
+## foo(1, Fob(x=1)) succeeded
+## foo(0, Fob(x=0)) succeeded
+## Error: foo arg -1 must be positive
+## Error: Fob arg -1 must be positive
+## Error: Fob arg -1 must be positive
+## foo(10, Fob(x=11)) succeeded
