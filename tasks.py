5022d3f31e632a366769b759cc9b9bca4e7f132d
Bruce Eckel <mindviewinc@gmail.com>
2025-04-09 09:52:13 -0600

    invoke tasks

--- tasks.py
+++ tasks.py
@@ -3,20 +3,37 @@
 """
 import sys
 from pathlib import Path
-from pybooktools.invoke_tasks import examples, validate, namespace
+from pybooktools.invoke_tasks import (
+    examples,
+    validate,
+    namespace,
+)
 from invoke import task
 from rich.console import Console
 from rich.prompt import Confirm
 
-markdown_chapters_path = Path("C:/git/ThinkingInTypes.github.io/Chapters")
+WIDTH = 55  # Width for code listings and comments
+
+markdown_chapters_path = Path(
+    "C:/git/ThinkingInTypes.github.io/Chapters"
+)
 target_path = Path("C:/git/ThinkingInTypes_Examples")
-temp_files = [Path("app.log"), Path("data.txt"), Path("other.txt")]
+temp_files = [
+    Path("app.log"),
+    Path("data.txt"),
+    Path("other.txt"),
+]
 
 console = Console()
 
 
-def confirm(message: str, default: bool = True) -> None:
-    if not Confirm.ask(f"[yellow]{message}[/yellow]", default=default):
+def confirm(
+    message: str, default: bool = True
+) -> None:
+    if not Confirm.ask(
+        f"[yellow]{message}[/yellow]",
+        default=default,
+    ):
         console.print("[red]Cancelled by user.[/red]")
         sys.exit(1)
 
@@ -34,9 +51,30 @@ def docformat(ctx) -> None:
     """
     Formats documentation strings in Python files.
     """
-    # ctx.run("docformatter -ri .")
-    ctx.run("docformatter --in-place --wrap-summaries 50 --wrap-descriptions 50 \
-    --make-summary-multi-line --pre-summary-newline -r .")
+    ctx.run(
+        f"docformatter --in-place --wrap-summaries {WIDTH} --wrap-descriptions {WIDTH} \
+    --make-summary-multi-line --pre-summary-newline -r ."
+    )
+
+
+@task
+def codeformat(ctx) -> None:
+    """
+    Formats code in Python files.
+    """
+    ctx.run(f"ruff format --line-length {WIDTH}")
+
+
+@task
+def update_example_output(ctx) -> None:
+    """
+    Updates embedded `##` outputs in examples.
+    """
+    confirm(
+        "Update embedded outputs with 'px -r .'?",
+        default=True,
+    )
+    ctx.run("px -r .")
 
 
 @task
@@ -46,13 +84,22 @@ def extract(ctx) -> None:
     examples directory.
     """
     ctx.run(f"mdvalid -d {markdown_chapters_path}")
-    confirm(f"WARNING: delete the examples in {target_path}?", default=False)
+    confirm(
+        f"WARNING: delete the examples in {target_path} extract new ones from book?",
+        default=False,
+    )
     if target_path.exists():  # Check repoclean to see if it already does this
         ctx.run(f"repoclean -a {target_path}")
     else:
-        print(f"Directory does not exist: {target_path}")
-    print(f"Running: mdextract -d {markdown_chapters_path} {target_path}")
-    ctx.run(f"mdextract -d {markdown_chapters_path} {target_path}")
+        print(
+            f"Directory does not exist: {target_path}"
+        )
+    print(
+        f"Running: mdextract -d {markdown_chapters_path} {target_path}"
+    )
+    ctx.run(
+        f"mdextract -d {markdown_chapters_path} {target_path}"
+    )
 
 
 @task
@@ -60,7 +107,10 @@ def inject(ctx):
     """
     Injects examples back into the chapter files.
     """
-    confirm("Inject examples back into book?", default=True)
+    confirm(
+        "Inject examples back into book?",
+        default=True,
+    )
     ctx.run(
         rf"mdinject -i {markdown_chapters_path} {target_path}"
     )
@@ -71,23 +121,14 @@ def a(ctx) -> None:
     """
     All workflow tasks: extract, run all scripts, update examples, validate, re-inject examples into book.
     """
-
-    confirm("Erase examples and extract new ones from book?", default=True)
     extract(ctx)
-
-    confirm("Run all examples?", default=True)
     examples(ctx)
-
-    confirm("Update embedded outputs with 'px -r .'?", default=True)
-    ctx.run("px -r .")
-
-    confirm("Validate example output?", default=True)
+    update_example_output(ctx)
     validate(ctx)
-
     inject(ctx)
-
-    console.print("[bold green]\n✅ Workflow completed successfully.[/bold green]")
-
+    console.print(
+        "[bold green]\n✅ Workflow completed successfully.[/bold green]"
+    )
     for file in temp_files:
         if file.exists():
             file.unlink()
@@ -99,3 +140,5 @@ namespace.add_task(extract)
 namespace.add_task(inject)
 namespace.add_task(z)
 namespace.add_task(docformat)
+namespace.add_task(codeformat)
+namespace.add_task(update_example_output)
