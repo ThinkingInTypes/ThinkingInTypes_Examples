156f3e4a7b7b3489cb653018fadbe72579c14368
Bruce Eckel <mindviewinc@gmail.com>
2025-04-09 11:51:08 -0600

    Ran 'invoke a'

--- book_utils/__init__.py
+++ book_utils/__init__.py
@@ -1,4 +1,3 @@
 # book_utils/__init__.py
 from .exception_catcher import Catch
-
 __all__ = ["Catch"]
--- book_utils/exception_catcher.py
+++ book_utils/exception_catcher.py
@@ -1,15 +1,12 @@
 # book_utils/exception_catcher.py
 """
-Context manager that catches exceptions and prints their
-error messages, and can be used as a callable via its
-__call__ method.
+Context manager that catches exceptions and prints their error messages,
+and can be used as a callable via its __call__ method.
 
-When used as a callable, argument evaluation must be delayed
-until inside the context manager in case argument evaluation
-raises an exception. To do this the function should be
-provided as a zero- argument callable. If the function takes
-arguments, it must be wrapped in a lambda to delay
-evaluation.
+When used as a callable, argument evaluation must be delayed until inside
+the context manager in case argument evaluation raises an exception.
+To do this the function should be provided as a zero-argument callable.
+If the function takes arguments, it must be wrapped in a lambda to delay evaluation.
 """
 
 from typing import Any, Callable, TypeVar
@@ -18,12 +15,11 @@ R = TypeVar("R")
 
 
 class Catch:
+
     def __enter__(self) -> "Catch":
         return self
 
-    def __exit__(
-        self, exc_type: Any, exc_value: Any, traceback: Any
-    ) -> bool:
+    def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> bool:
         # __exit__ is only called if an exception escapes the block.
         if exc_type is not None:
             print(f"Error: {exc_value}")
