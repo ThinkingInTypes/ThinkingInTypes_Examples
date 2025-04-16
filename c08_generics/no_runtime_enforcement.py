4d6e542502dd9c3c082fc3ebd173cf33350dadc5
Bruce Eckel <mindviewinc@gmail.com>
2025-04-16 16:22:50 -0600

    etc

--- c08_generics/no_runtime_enforcement.py
+++
@@ -1,8 +0,0 @@
-# no_runtime_enforcement.py
-from box import Box
-
-## 123
-## Python
-
-box: Box[int] = Box(123)
-box.content = "not an int"  # type: ignore
--- c10_structural_typing/logger_protocol.py
+++ c10_structural_typing/logger_protocol.py
@@ -1,28 +1,40 @@
 # logger_protocol.py
 from typing import Protocol
+from pathlib import Path
+import tempfile
+import os
+from contextlib import AbstractContextManager
 
 
 class Logger(Protocol):
     def log(self, message: str) -> None: ...
 
 
-class FileLogger:
+class FileLogger(AbstractContextManager):
     """
-    Concrete logger that writes to a file.
+    Logger that writes to a temporary file.
+    Automatically cleans up with the context manager.
     """
 
-    def __init__(self, filename: str):
-        self.filename = filename
+    def __init__(self):
+        self._temp_file = tempfile.NamedTemporaryFile(delete=False, mode="a", encoding="utf-8")
+        self.filename = Path(self._temp_file.name)
 
     def log(self, message: str) -> None:
-        with open(self.filename, "a") as f:
-            f.write(message + "\n")
+        self._temp_file.write(message + "\n")
+        self._temp_file.flush()
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        self._temp_file.close()
+        try:
+            os.remove(self.filename)
+        except FileNotFoundError:
+            pass
 
 
 class ListLogger:
     """
-    Concrete logger that stores messages in a list (e.g.,
-    for testing).
+    Logger that stores messages in a list
     """
 
     def __init__(self):
@@ -33,21 +45,19 @@ class ListLogger:
 
 
 def run_process(
-        task_name: str, logger: Logger
+    task_name: str, logger: Logger
 ) -> None:
     logger.log(f"Starting {task_name}")
     # Perform the task ...
     logger.log(f"Finished {task_name}")
 
 
-# Using the run_process with different loggers
-run_process(
-    "DataCleanup", FileLogger("app.log")
-)  # logs to file
+with FileLogger() as file_logger:
+    run_process("DataCleanup", file_logger)
+    print(f"log file: {file_logger.filename}")
+    print(file_logger.filename.read_text(encoding="utf-8"))
+
+# logs to list in memory:
 test_logger = ListLogger()
-run_process(
-    "DataCleanup", test_logger
-)  # logs to list in memory
+run_process("DataCleanup", test_logger)
 print("Captured logs:", test_logger.messages)
-## Captured logs: ['Starting DataCleanup',
-## 'Finished DataCleanup']
