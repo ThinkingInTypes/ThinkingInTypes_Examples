8466b2facc1f8d4e48b4d78917bed693188e4508
Bruce Eckel <mindviewinc@gmail.com>
2025-04-16 18:58:44 -0600

    Create dataclass_initvar.py

---
+++ c05_custom_types/dataclass_initvar.py
@@ -0,0 +1,23 @@
+# dataclass_initvar.py
+# pyright: reportAttributeAccessIssue=false
+# mypy: disable-error-code="attr-defined"
+from dataclasses import dataclass, InitVar
+from book_utils import Catch
+
+
+@dataclass
+class Book:
+    title: str
+    author: str
+    condition: InitVar[str]
+    shelf_id: int | None = None
+
+    def __post_init__(self, condition):
+        if condition == "Unacceptable":
+            self.shelf_id = None
+
+
+print(b := Book("Emma", "Jane Austen", "Good", 11))
+# "condition" doesn't exist outside __init__ or __post_init__:
+with Catch():
+    print(b.condition)  # noqa
