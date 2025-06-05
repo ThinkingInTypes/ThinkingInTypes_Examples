63857edc05fddaf9397f2858fda18446123e8bd7
Bruce Eckel <mindviewinc@gmail.com>
2025-06-05 11:28:08 -0600

    Update smalltalk_object.py

--- z11_appendix_dynamic_smalltalk_vs_static_typing/smalltalk_object.py
+++ z11_appendix_dynamic_smalltalk_vs_static_typing/smalltalk_object.py
@@ -1,4 +1,6 @@
 # smalltalk_object.py
+
+
 class SmalltalkObject:
     def __getattr__(self, name):
         def handler(*args, **kwargs):
@@ -7,6 +9,14 @@ class SmalltalkObject:
         return handler
 
     def not_found(self, message, *args, **kwargs):
+        args_str = ", ".join(repr(arg) for arg in args)
+        kwargs_str = ", ".join(
+            f"{k}={v!r}" for k, v in kwargs.items()
+        )
+        all_args = ", ".join(
+            filter(None, [args_str, kwargs_str])
+        )
+
         print(
-            f"{self.__class__.__name__}: '{message}' not found"
+            f"{self.__class__.__name__}: {message}({all_args}) not found"
         )
